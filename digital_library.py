566d2b74ff42a1bf55de273ff5ea16aece68630c
Brady Richardson <>
2024-10-01 16:45:58 -0600

    test length

--- digital_library.py
+++ digital_library.py
@@ -13,7 +13,7 @@ class Book:
         self.book_id = book_id
     
     def is_long(self):
-        return self.pages > 1755
+        return self.pages > 2000
 
     def get_age(self):
         return datetime.datetime.now().year - self.published_year
